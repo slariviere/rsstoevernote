53036c1b82bc7af9864a9732e043f92073d0be2f
Sébastien Larivière <sebastien@lariviere.me>
2013-12-21 18:40:33 -0500

    Add default database name

--- sql/createdb.sh
+++ sql/createdb.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-echo $rsstoevernotedb
+if [ -z "$rsstoevernotedb" ]; then
+	export rsstoevernotedb=rsstoevernote/rsstoevernote.db
+fi
+
+if [ -f "$rsstoevernotedb" ]; then
+	echo "[+] Deleting old database"
+	rm "$rsstoevernotedb"
+fi
+echo "[+] Create $rsstoevernotedb database"
 sqlite3 ${rsstoevernotedb} < sql/rsstoevernoteDB.sql
